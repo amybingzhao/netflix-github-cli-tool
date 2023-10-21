90d42fcc9c54d203c9059d6b0bd9c92dc7697e67
Bing <azhao96@gmail.com>
2023-10-21 16:12:19 -0400

    handle ties

--- utilities/repo_utilities.py
+++ utilities/repo_utilities.py
@@ -5,15 +5,6 @@ from utilities.cache_utilities import GithubDataCache
 from typing import Callable
 from models.repo_data import RepoData
 
-def _get_value_for_basic_criteria(repo: Repository.Repository, criteria: Criteria, cache: GithubDataCache, fetch_method: Callable[[Repository.Repository], int]) -> int | float:
-    cached_value = cache.try_get_data_for_repo(repo.name, criteria)
-    if cached_value is not None:
-        return cached_value
-    else:
-        updated_value = fetch_method(repo)
-        cache.update_data_for_repo(repo.name, criteria, updated_value)
-        return updated_value
-
 def _get_value_for_criteria(repo: Repository.Repository, criteria: Criteria, cache: GithubDataCache) -> int | float:
     repo_data = cache.try_get_data_for_repo(repo.name)
     
@@ -33,13 +24,14 @@ def get_top_repos_by_criteria(repos: PaginatedList.PaginatedList[Repository.Repo
         value = _get_value_for_criteria(repo, criteria, cache)
         if len(top_repos_with_value) < n:
             # if we have fewer than n items in our top_repos list, add this repo in
-            top_repos_with_value.append((value, repo))
+            top_repos_with_value.append((value, repo.name, repo))
             heapq.heapify(top_repos_with_value)
         else:
             # if we have more than n items, we check if this repo has a higher value than the 
             # smallest (value, repo) pair currently in our top_repos heap. if it does, then 
             # remove the smallest value and push the new one onto our heap
-            if value > top_repos_with_value[0][0]:
-                heapq.heapreplace(top_repos_with_value, (value, repo))
+            (min_value, min_value_repo_name, _) = top_repos_with_value[0]
+            if value > min_value or value == min_value and repo.name > min_value_repo_name:
+                heapq.heapreplace(top_repos_with_value, (value, repo.name, repo))
 
-    return [repo_with_value[1] for repo_with_value in top_repos_with_value]
+    return [repo_with_value[2] for repo_with_value in top_repos_with_value]
